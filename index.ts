957b98a071e2ee1457df0c3c8cc978e45dd55465
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-11-13 16:20:32 +0100

    env port

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app: Application = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,6 +20,6 @@ app.get("/", (req, res) => {
   res.send("Hello Express app");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
